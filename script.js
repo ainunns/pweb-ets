becd5557a7dff268fd2137ecd58c907df32a11c4
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-10-16 09:22:11 +0700

    fix: data fetching

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ const allCurrencies = async () => {
       "http://apilayer.net/api/live?access_key=ff0c3c1268db0a8bdbfc31b4d6ab09d1&pairs=EURUSD,EURGBP,GBPUSD,USDJPY,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR"
     );
     const data = await res.json();
+    const currencies = data.quotes;
 
     const contents = document.getElementById("content");
 
-    for (let x in data) {
+    for (let x in currencies) {
       const el = document.createElement("div");
       el.classList.add(
         "col-12",
@@ -37,8 +38,9 @@ const allCurrencies = async () => {
       p.innerText = `${x}`;
       const h5 = document.createElement("h5");
       h5.classList.add("card-text");
-      h5.innerText = `${x}`;
-      el2.appendChild(p, h5);
+      h5.innerText = `${currencies[x]}`;
+      el2.appendChild(p);
+      el2.appendChild(h5);
       el.appendChild(el2);
       contents.appendChild(el);
     }
